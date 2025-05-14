4c2db29e73b32d5eedf926934f52b41541d955f2
BFBacchi <bfbacchi@gmail.com>
2025-05-13 21:57:00 -0300

    se agregan variables de entorno para no exponer apikey

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCzRLf6tTuHQu0g_Gq-iPPxOgce-7cx5Rk",
-  authDomain: "project-utn-class.firebaseapp.com",
-  projectId: "project-utn-class",
-  storageBucket: "project-utn-class.firebasestorage.app",
-  messagingSenderId: "301009935667",
-  appId: "1:301009935667:web:7b20b971569d3c6173b246",
-  measurementId: "G-J7THHG7G7K"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
